79a3525085dcdf01cdd74cc6b5ce1243bb9c5b54
Robertas Setkus <robertas.setkus@gmail.com>
2017-10-04 23:54:49 +0100

    initial challenge 1 commit

---
+++ project/src/main/java/lt/setkus/cryptopals/Challenge1.java
@@ -0,0 +1,4 @@
+package lt.setkus.cryptopals;
+
+public class Challenge1 {
+}
---
+++ project/src/test/java/lt/setkus/cryptopals/Challenge1Test.java
@@ -0,0 +1,7 @@
+package lt.setkus.cryptopals;
+
+import static org.junit.Assert.*;
+
+public class Challenge1Test {
+
+}
