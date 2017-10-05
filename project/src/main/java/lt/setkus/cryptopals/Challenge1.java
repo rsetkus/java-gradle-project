6ef01b6840a352430e95a51baec37e42798a5aee
Robertas Setkus <robertas.setkus@gmail.com>
2017-10-05 22:56:04 +0100

    test added

--- project/src/main/java/lt/setkus/cryptopals/Challenge1.java
+++ project/src/main/java/lt/setkus/cryptopals/Challenge1.java
@@ -1,4 +1,18 @@
 package lt.setkus.cryptopals;
 
+import static java.lang.System.out;
+
+/**
+ * http://www.developer.com/java/other/article.php/3386271/Understanding-Base64-Data.htm
+ */
 public class Challenge1 {
+
+    public static String encodeBase64(String input) {
+        out.println(input);
+
+        int hex = Character.digit(input.charAt(0), 16);
+        out.println("hex: " + hex);
+
+        return null;
+    }
 }
--- project/src/test/java/lt/setkus/cryptopals/Challenge1Test.java
+++ project/src/test/java/lt/setkus/cryptopals/Challenge1Test.java
@@ -1,7 +1,13 @@
 package lt.setkus.cryptopals;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class Challenge1Test {
 
+    @Test
+    public void shouldEncodeProvidedStringToBase64() throws Exception {
+        assertEquals(Challenge1.encodeBase64("4"), "S");
+    }
 }
